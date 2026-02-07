9bae5f62cc0e1210ad9c7ac42914d28bbbb72de2
Tommaso R. Marena <112788717+ChessEngineUS@users.noreply.github.com>
2026-02-07 18:14:20 -0500

    Add configuration files and update model imports

--- hqpf/models/__init__.py
+++ hqpf/models/__init__.py
@@ -1,15 +1,15 @@
-"""Models module containing quantum and classical components."""
+"""Model implementations."""
 
-from hqpf.models.hybrid_model import HybridQuantumModel
-from hqpf.models.quantum_hamiltonian import QuantumHamiltonian
-from hqpf.models.vqe_solver import VQESolver
-from hqpf.models.structure_generator import StructureGenerator
-from hqpf.models.surrogate import SurrogateModel
+from .quantum_hamiltonian import QuantumHamiltonian
+from .vqe_solver import VQESolver
+from .structure_generator import StructureGenerator
+from .surrogate import SurrogateModel
+from .hybrid_model import HybridModel
 
 __all__ = [
-    "HybridQuantumModel",
-    "QuantumHamiltonian",
-    "VQESolver",
-    "StructureGenerator",
-    "SurrogateModel",
+    'QuantumHamiltonian',
+    'VQESolver',
+    'StructureGenerator',
+    'SurrogateModel',
+    'HybridModel'
 ]
